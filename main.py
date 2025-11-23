e2ec4f58efb983995ff479bcec83554987fffb37
Luigiday <88960729+luigiday@users.noreply.github.com>
2025-11-23 19:00:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Example usage of the w2n_app module to launch a web application as a native app
+# Your web app files should be located in the 'web' directory in the project root
 
 import w2n_app
 
