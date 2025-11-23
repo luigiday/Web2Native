b1b5da6210199ebf9ab024add46f9937dfeea355
Luigiday <88960729+luigiday@users.noreply.github.com>
2025-11-23 18:58:43 +0100

    edit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import app
+# Example usage of the w2n_app module to launch a web application as a native app
 
-app.launch(filename='index.html')
+import w2n_app
+
+w2n_app.launch(filename='index.html')
